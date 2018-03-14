02996899423846826f7201c49cab35f54028b0c0
Administrator <Administrator@TLQV_DESKTOP>
2018-03-14 14:11:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace HPTTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumVar = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.BackColor = System.Drawing.Color.Red;
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 4.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.exitBtn.Location = new System.Drawing.Point(554, -2);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(20, 20);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "X";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click_1);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.FlatAppearance.BorderSize = 0;
+            this.btnCalc.Location = new System.Drawing.Point(12, 83);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 32);
+            this.btnCalc.TabIndex = 14;
+            this.btnCalc.Text = "Nhập";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click_2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(9, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Số biến";
+            // 
+            // txtNumVar
+            // 
+            this.txtNumVar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNumVar.Location = new System.Drawing.Point(85, 54);
+            this.txtNumVar.Margin = new System.Windows.Forms.Padding(0);
+            this.txtNumVar.Name = "txtNumVar";
+            this.txtNumVar.Size = new System.Drawing.Size(24, 15);
+            this.txtNumVar.TabIndex = 13;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(121, 51);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(451, 184);
+            this.flowLayoutPanel1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 30);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Giải PT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(118, 25);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 17);
+            this.lbResult.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(9, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Trần Hải Dương-ĐTHK51";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(575, 263);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumVar);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.exitBtn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumVar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HPTTT
+{
+    public partial class Form1 : Form
+    {
+        int labelLength;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalc_Click_2(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            var numVar = int.Parse(txtNumVar.Text);
+            var tmpStr = "X" + numVar.ToString() + " x ";
+            labelLength = tmpStr.Length * 2 + 25;
+            flowLayoutPanel1.Width = (numVar + 1) * (labelLength + 37);
+            flowLayoutPanel1.Height = (numVar + 1) * 20;
+            int count = 0;
+            for (int i = 0; i < numVar; i++)
+            {
+                for (int j = 0; j < numVar; j++)
+                {
+                    Label tmpLabel = new Label();
+                    tmpLabel.Text = "X" + (j+1).ToString() + " x ";
+                    tmpLabel.Font = new Font("Arial", 10, FontStyle.Regular);
+                    tmpLabel.Size = new Size(labelLength, 15);
+                    tmpLabel.Padding = new System.Windows.Forms.Padding(0);
+                    tmpLabel.Margin = new System.Windows.Forms.Padding(0);
+                    ///////                    
+                    TextBox tmpTxt = new TextBox();
+                    tmpTxt.Text = "0";
+                    tmpTxt.BorderStyle = BorderStyle.None;
+                    tmpTxt.Size = new Size(30, 15);
+                    tmpTxt.TabIndex = count;
+                    tmpTxt.Name = i.ToString() + "," + j.ToString();
+                    count++;
+                    flowLayoutPanel1.Controls.Add(tmpLabel);
+                    flowLayoutPanel1.Controls.Add(tmpTxt);
+                }
+                Label addLabel = new Label();
+                addLabel.Text = @" = ";
+                addLabel.Font = new Font("Arial", 10, FontStyle.Regular);
+                addLabel.Size = new Size(labelLength, 15);
+                addLabel.Padding = new System.Windows.Forms.Padding(0);
+                addLabel.Margin = new System.Windows.Forms.Padding(0);
+                ///////                    
+                TextBox addTxt = new TextBox();
+                addTxt.BorderStyle = BorderStyle.None;
+                addTxt.Text = "0";
+                addTxt.Size = new Size(30, 15);
+                addTxt.TabIndex = count;
+                addTxt.Name = i.ToString() + "," + numVar.ToString();
+                count++;
+                flowLayoutPanel1.Controls.Add(addLabel);
+                flowLayoutPanel1.Controls.Add(addTxt);
+            }
+
+        }
+
+        void Calc()
+        {
+            var numVar = int.Parse(txtNumVar.Text);
+            string[,] Kteam = new string[2, 3];
+            double[,] matrix = new double[numVar, numVar + 1];
+            foreach (var item in flowLayoutPanel1.Controls)
+            {
+                if (item is TextBox)
+                {
+                    TextBox tmp = item as TextBox;
+                    //int index= tmp.TabIndex;
+                    string[] name = new string[2];
+                    name = tmp.Name.Split(',');
+                    //int a = index / numVar;
+                    matrix[Convert.ToInt32(name[0]), Convert.ToInt32(name[1])] = double.Parse(tmp.Text);
+                }
+
+            }
+            for (int i = 0; i < numVar; i++)
+            {
+                for (int k = i + 1; k < numVar; k++)
+                {
+                    if (Math.Abs(matrix[i, i]) < Math.Abs(matrix[k, i]))
+                    {
+                        for (int j = 0; j <= numVar; j++)
+                        {
+                            double tmp = matrix[i, j];
+                            matrix[i, j] = matrix[k, j];
+                            matrix[k, j] = tmp;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < numVar - 1; i++)
+            {
+                for (int k = i + 1; k < numVar; k++)
+                {
+                    double t = matrix[k, i] / matrix[i, i];
+                    for (int j = 0; j <= numVar; j++)
+                    {
+                        matrix[k, j] = matrix[k, j] - t * matrix[i, j];
+                    }
+                }
+            }
+            double[] result = new double[numVar];
+            for (int i = numVar - 1; i >= 0; i--)
+            {
+                result[i] = matrix[i, numVar];
+                for (int j = i + 1; j < numVar; j++)
+                {
+                    if (j != i)
+                    {
+                        result[i] = result[i] - matrix[i, j] * result[j];
+                    }
+                }
+                result[i] = result[i] / matrix[i, i];
+                result[i] = Math.Round(result[i], 3);               
+            }
+            string strResult = "Kết quả:  ";
+            for (int i = 0; i < numVar; i++)
+            {
+                strResult += "X" +(i+1).ToString() + " = " + result[i].ToString() + "  ";
+            }
+            lbResult.Text = strResult;
+        }
+
+        private void exitBtn_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calc();
+        }
+    }
+}
